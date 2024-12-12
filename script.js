eb37a1e4de6c738b950b8718680d117d96ccc8ef
danieletie <etiedaniel@gmail.com>
2024-12-12 20:05:00 +0100

    Create script.js file
    - add four functions to add, subtract, multiply and divide

---
+++ script.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+console.log(sum (2, 2)) //should return 4
+
+console.log(subtract (2, 2)) //should return 0
+
+console.log(multiply (2, 2)) //should return 4
+
+console.log(divide (2, 2)) //should return 1
