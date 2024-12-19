064c957cd2cb07d404b2368ef4654333a763d436
danieletie <etiedaniel@gmail.com>
2024-12-19 07:16:04 +0100

    Create variables and functions
    - create three values for two numbers and one operator
    - add operate function to call two numbers and any of the four math
      operators by calling their respective functions

--- script.js
+++ script.js
@@ -14,6 +14,26 @@ function divide(a, b) {
   return a / b
 }
 
+let firstNum
+
+let secondNum
+
+let operand
+
+function operate(firstNum, operand, secondNum) {
+  if (operand === '+') {
+    return sum(firstNum, secondNum)
+  } else if (operand === '-') {
+    return subtract(firstNum, secondNum)
+  } else if (operand === '*') {
+    return multiply(firstNum, secondNum)
+  } else if (operand === '/') {
+    return divide(firstNum, secondNum)
+  }
+} 
+
+
+
 console.log(sum (2, 2)) //should return 4
 
 console.log(subtract (2, 2)) //should return 0
@@ -21,3 +41,11 @@ console.log(subtract (2, 2)) //should return 0
 console.log(multiply (2, 2)) //should return 4
 
 console.log(divide (2, 2)) //should return 1
+
+console.log(operate(4, '+', 4)) //should return 8
+
+console.log(operate(20, '-', 6)) //should return 14
+
+console.log(operate(20, '*', 6)) //should return 120
+
+console.log(operate(20, '/', 2)) //should return 10
