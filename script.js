591d8073bd74d2733f54a6a28d84375d830e8c03
danieletie <etiedaniel@gmail.com>
2024-12-22 15:49:04 +0100

    Modify HTML and CSS files
    - add value to reset button in html
    - add functionality to js file when reset button is clicked
    - improve logic of calculator to work no matter what number buttons or
      operator button is clicked.

--- script.js
+++ script.js
@@ -49,31 +49,62 @@ let numbers = document.querySelectorAll('button');
 // }));
 
 
+function initVariable() {
+  firstNum = '';
+  secondNum = '';
+  operand = '';
+}
+
 numbers.forEach((button) => button.addEventListener('click', () => {
-  if (button.value === '=' && typeof firstNum === 'string' && firstNum.length === 0){
+
+  if (button.value === 'reset') {
+    console.log('You have reset everything!');
+    initVariable();
+    display.textContent = '0';
+    console.log(`First Number is '${firstNum}', second number is '${secondNum}', operator is '${operand}'`)
+  }
+
+  if (button.value === '=' && firstNum.length === 0){
     console.log(`I'm doing nothing cause you clicked '=' when First Number is a ${typeof firstNum} and length is ${firstNum.length}`)
-  } else if (button.value === '=' && (typeof firstNum === 'string' && firstNum.length !== 0) && (typeof secondNum === 'string' && secondNum.length !== 0)) {
+  } else if (button.value === '=' && firstNum.length !== 0 && secondNum.length !== 0) {
     console.log(`First number is '${firstNum}' second number is '${secondNum}' and operator is '${operand}'`);
     console.log(operate(Number(firstNum), operand, Number(secondNum)));
     display.textContent = operate(Number(firstNum), operand, Number(secondNum));
-    firstNum = '';
-    secondNum = '';
-    operand = '';
+    initVariable();
   };
+
+ 
 
 
   if ((button.value === '+' || 
       button.value === '-' ||
       button.value === '*' ||
       button.value === '/') && 
-      typeof firstNum === 'string' &&
       firstNum.length === 0) {
         console.log(`I'm doing nothing cause you clicked '${button.value}' when First Number is a ${typeof firstNum} and length is ${firstNum.length}`)
+  } else if ((button.value === '+' || 
+              button.value === '-' ||
+              button.value === '*' ||
+              button.value === '/') && 
+              firstNum.length !== 0 && operand.length === 0 && secondNum.length === 0) {
+                console.log('assign button value')
+                operand = button.value;
+                console.log(operand);
   };
+
+
+
+
+
+
+
 
   if (button.value === '0' && typeof firstNum === 'string' && firstNum.length === 0) {
     console.log(`I'm doing nothing cause you clicked '${button.value}' when First Number is a ${typeof firstNum} and length is ${firstNum.length}`)
   };
+
+
+
 
 
 
@@ -98,91 +129,53 @@ numbers.forEach((button) => button.addEventListener('click', () => {
         console.log(firstNum);
         console.log(firstNum.length);
   };
-
-  if ((button.value === '+' || 
-        button.value === '-' ||
-        button.value === '*' ||
-        button.value === '/') && 
-        typeof firstNum === 'string' &&
-        firstNum.length !== 0) {
-    operand = button.value;
-    console.log(`We've assigned operator as '${button.value}' because there's a First Number now and it's ${firstNum}`);
-    console.log(secondNum);
+  if ((typeof operand === 'string' && operand.length !== 0)  &&
+    ((button.value === '1' ||
+      button.value === '2' ||
+      button.value === '3' ||
+      button.value === '4' ||
+      button.value === '5' ||
+      button.value === '6' ||
+      button.value === '7' ||
+      button.value === '8' ||
+      button.value === '9' ||
+      (button.value === '0' &&  typeof secondNum === 'string' && secondNum.length !== 0)))) {
+        console.log(`You clicked '${button.value}'`); 
+        display.textContent = secondNum;
+        display.textContent += button.value;
+        secondNum = display.textContent;
+        console.log(secondNum);
   };
+  
+if ((button.value === '+' || 
+            button.value === '-' ||
+            button.value === '*' ||
+            button.value === '/') &&
+            firstNum.length !== 0 && operand.length !== 0 && secondNum.length !== 0){
+              console.log(`First num is '${firstNum}' secondNum is '${secondNum}' operand is '${operand}'!`)
+              console.log('this is new');
+              display.textContent = (operate(Number(firstNum), operand, Number(secondNum)));
+              firstNum = (operate(Number(firstNum), operand, Number(secondNum)));
+              operand = button.value;
+              secondNum = ''
+};
+  
 
 
-  if (button.value === '0' && typeof firstNum === 'string' && firstNum.length !== 0 && typeof operand === 'string' && operand.length !== 0) {
+  if (button.value === '0' && (typeof firstNum === 'string' && firstNum.length !== 0) && (typeof operand === 'string' && operand.length !== 0)) {
     console.log(`I'm just checking`)
   }
 
-  if ((typeof operand === 'string' && operand.length !== 0) &&
-      ((button.value === '1' ||
-        button.value === '2' ||
-        button.value === '3' ||
-        button.value === '4' ||
-        button.value === '5' ||
-        button.value === '6' ||
-        button.value === '7' ||
-        button.value === '8' ||
-        button.value === '9' ||
-        (button.value === '0' &&  typeof secondNum === 'string' && secondNum.length !== 0)))) {
-          console.log(`You clicked '${button.value}'`);
-          display.textContent = secondNum;
-          display.textContent += button.value;
-          secondNum = display.textContent;
-  };
 
 
 
-//   if ((button.value === '+' || 
-//     button.value === '-' ||
-//     button.value === '*' ||
-//     button.value === '/') && 
-//     (typeof firstNum === 'string' &&
-//     firstNum.length !== 0) && 
-//     (typeof seconNum === 'string' &&
-//       secondNum.length !== 0
-//     )) {
-//   operand = button.value;
-//   console.log(`We've assigned operator as '${button.value}' because there's a First Number now and it's ${firstNum}`);
-//   console.log(secondNum);
-// };
+
+
 
 }));
 
 
 
-
-
-
-
-
-
-
-
-
-
-// document.querySelectorAll('#opr').forEach((button) => button.addEventListener('click', () => {
-//   if (button.value === '+'){
-//     operand = '+';
-//     console.log(operand);
-//   };
-//   display.textContent = secondNum;
-//   display.textContent += button.value;
-//   secondNum = display.textContent;
-//   console.log(`Second number is ${secondNum}`);
-//   console.log(typeof secondNum);
-// }));
-
-
-
-// function variable1 () {
-//   if (typeof firstNum === 'string' && firstNum.length === '0'){
-//     display.textContent += button.value;
-//     firstNum = display.textContent;
-//     return firstNum;
-//   }
-// }
 
 
 
